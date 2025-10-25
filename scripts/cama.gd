96ad3c10ccaf9da59ce9d58eb2bb56730bb55cd4
holatrolaalola <hugo.bosque@estudiantat.upc.edu>
2025-10-25 16:40:04 +0200

    pruebas

--- scripts/cama.gd
+++ scripts/cama.gd
@@ -2,13 +2,15 @@ extends Area2D
 
 @onready var exclamacion: Sprite2D = $Exclamacion
 
+const ESTADISTICAS_DIA = preload("res://scenes/cinemarografia/Estadisticas_Dia.tscn")
 
-# @onready var pantalla_resumen = preload("res://ui/PantallaResumenDia.tscn")
 
 var player_in_area = false
 
-
-
+func _ready():
+	exclamacion.visible = false
+	connect("body_entered", Callable(self, "_on_body_entered"))
+	connect("body_exited", Callable(self, "_on_body_exited"))
 
 func _on_body_entered(area) -> void:
 	if area.name == "Player":
@@ -21,19 +23,11 @@ func _on_body_exited(area) -> void:
 		player_in_area = false
 		exclamacion.visible = false
 		
-func _ready():
-	exclamacion.visible = false
+
 
 
 func _process(delta):
 	if player_in_area and Input.is_action_just_pressed("ui_accept"):
 		GameManager.dia += 1
-		#_dormir()
-
-# func _dormir():
-	#var player = get_parent().get_node_or_null("Player")
-	#if player:
-		#player.set_movement_enabled(false)
-
-#	var resumen = pantalla_resumen.instantiate()
-#	get_tree().get_root().add_child(resumen)
+		get_tree().change_scene_to_packed(ESTADISTICAS_DIA)
+	
