b54160f82e14b4fe783bad29b0736b23e3d30c04
holatrolaalola <hugo.bosque@estudiantat.upc.edu>
2025-10-26 18:04:33 +0100

    collision cura

--- scripts/Game_Manager.gd
+++ scripts/Game_Manager.gd
@@ -7,7 +7,7 @@ var has_done_dialogue1 = false
 var dia: int = 1
 var vida_cura: int = 100
 var satisfacion_cura: int = 0
-var preocupacion_cura: int = 40
+var preocupacion_cura: int = 0
 
 
 signal dia_cambiado(nuevo_dia: int)
