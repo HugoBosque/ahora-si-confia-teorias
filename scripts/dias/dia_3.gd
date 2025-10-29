a1a4fe880a4de8559360cf6c53aa3b473fdacc7b
holatrolaalola <hugo.bosque@estudiantat.upc.edu>
2025-10-29 19:09:40 +0100

    trabajarrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr

--- scripts/dias/dia_3.gd
+++ scripts/dias/dia_3.gd
@@ -10,5 +10,7 @@ func _process(delta):
 	$preocupacion_madre.text = "Preocupacion de la madre: " + str(GameManager.preocupacion_madre)
 	$satisfaccion_madre.text = "Satisfaccion de la madre: " + str(GameManager.satisfacion_madre)
 	$vida_madre.text = "Vida de la madre: " + str(GameManager.vida_madre)
-
+	$preocupacion_cura2.text = "Preocupación del cura: " + str(GameManager.preocupacion_cura)
+	$satisfaccion_cura2.text = "Satisfaccion del cura: " + str(GameManager.satisfacion_cura)
+	$vida_cura2.text = "Vida del cura: " + str(GameManager.vida_cura)
 	
