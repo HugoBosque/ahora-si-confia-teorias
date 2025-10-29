37b35d8b5716da47a7131c7ac9a3cbcf611ec807
HugoBosque <hugo.bosque@estudiantat.upc.edu>
2025-10-29 10:50:22 +0100

    me duele ya la espalda

--- scripts/scripts globales/Global.gd
+++ scripts/scripts globales/Global.gd
@@ -1,7 +1,25 @@
 extends Node
 
 var dia1_hablar_cura = false
+var dia2_hablar_cura = false
+var dia3_hablar_cura = false
+var dia4_hablar_cura = false
+var dia5_hablar_cura = false
+var dia6_hablar_cura = false
+var dia7_hablar_cura = false
+var dia2_hablar_madre = false
+var dia3_hablar_madre = false
+var dia4_hablar_madre = false
+var dia5_hablar_madre = false
+var dia6_hablar_madre = false
+var dia7_hablar_madre = false
 var dia1_halbar_esceptico = false
+var dia2_halbar_esceptico = false
+var dia3_halbar_esceptico = false
+var dia4_halbar_esceptico = false
+var dia5_halbar_esceptico = false
+var dia6_halbar_esceptico = false
+var dia7_halbar_esceptico = false
 var has_seen_presi_scene: bool = false
 
 
