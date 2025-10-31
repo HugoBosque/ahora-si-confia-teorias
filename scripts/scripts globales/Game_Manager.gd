e43765d8078b41426f2bc04b7ff83aaa32e62e47
holatrolaalola <hugo.bosque@estudiantat.upc.edu>
2025-10-31 23:25:34 +0100

    si

--- scripts/scripts globales/Game_Manager.gd
+++ scripts/scripts globales/Game_Manager.gd
@@ -6,6 +6,7 @@ var has_done_dialogue1 = false
 var has_done_cinematic1 = false
 var has_animation_done = false
 var dia: int = 1
+
 var vida_cura: int = 100
 var satisfacion_cura: int = 0
 var preocupacion_cura: int = 0
@@ -18,7 +19,25 @@ var vida_madre: int = 100
 var satisfacion_madre: int = 0
 var preocupacion_madre: int = 0
 
+var vida_viktor: int = 100
+var satisfacion_viktor: int = 0
+var preocupacion_viktor: int = 0
 
+var vida_viudo: int = 100
+var satisfacion_viudo: int = 0
+var preocupacion_viudo: int = 0
+
+var vida_medico: int = 100
+var satisfacion_medico: int = 0
+var preocupacion_medico: int = 0
+
+var vida_nina: int = 100
+var satisfacion_nina: int = 0
+var preocupacion_nina: int = 0
+
+var vida_mikhail: int = 100
+var satisfacion_mikhail: int = 0
+var preocupacion_mikhail: int = 0
 
 signal dia_cambiado(nuevo_dia: int)
 
