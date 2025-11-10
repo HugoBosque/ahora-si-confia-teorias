45b65f4e6ae7673727c1f6500c68c56fa7046677
HugoBosque <hugo.bosque@estudiantat.upc.edu>
2025-11-10 15:58:48 +0100

    dialogos

--- scripts/pers/minero.gd
+++ scripts/pers/minero.gd
@@ -2,21 +2,20 @@ extends Area2D
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 
-const MIKHAIL_DIA_1_1 = preload("uid://buvoybyeof2ym")
-const MIKHAIL_DIA_1_2 = preload("uid://sun5ej4wl11x")
-const MIKHAIL_DIA_2_1 = preload("uid://c8y7142mc01p1")
-const MIKHAIL_DIA_2_2 = preload("uid://bmf2lwbswweuu")
-const MIKHAIL_DIA_3_1 = preload("uid://cjb4n2h64fgq5")
-const MIKHAIL_DIA_3_2 = preload("uid://bg2otvj6xhwww")
-const MIKHAIL_DIA_4_1 = preload("uid://dldw7nu3nxdqd")
-const MIKHAIL_DIA_4_2 = preload("uid://dvpeewqdb5cso")
-const MIKHAIL_DIA_5_1 = preload("uid://bsablhwsj8aa4")
-const MIKHAIL_DIA_5_2 = preload("uid://qv150t1x3ipn")
-const MIKHAIL_DIA_6_1 = preload("uid://cu3fgflephau0")
-const MIKHAIL_DIA_6_2 = preload("uid://jerwqhjgxonl")
-const MIKHAIL_DIA_7_1 = preload("uid://ilf0msyq8fft")
-const MIKHAIL_DIA_7_2 = preload("uid://dhva7f56c6i5s")
-
+const YERIK_DIA_1_1 = preload("uid://lvjvxm0mqgyh")
+const YERIK_DIA_1_2 = preload("uid://cewsl2o358ddt")
+const YERIK_DIA_2_1 = preload("uid://cetmwkjo7tj11")
+const YERIK_DIA_2_2 = preload("uid://cgud6e8dhwued")
+const YERIK_DIA_3_1 = preload("uid://bfprlg4ihermo")
+const YERIK_DIA_3_2 = preload("uid://d1qwspfuhvdb3")
+const YERIK_DIA_4_1 = preload("uid://orojqtrc5gsv")
+const YERIK_DIA_4_2 = preload("uid://tks2gdmpdovf")
+const YERIK_DIA_5_1 = preload("uid://cct0p3lttordi")
+const YERIK_DIA_5_2 = preload("uid://cr1ehdc05b3vd")
+const YERIK_DIA_6_1 = preload("uid://b1jovwl8ro4dx")
+const YERIK_DIA_6_2 = preload("uid://i5wq75o3gons")
+const YERIK_DIA_7_1 = preload("uid://mmp7nvs7vk3y")
+const YERIK_DIA_7_2 = preload("uid://lyfsd8cgiwpn")
 
 
 
@@ -29,40 +28,40 @@ func _ready() -> void:
 func _process(delta):
 	if player_close and Input.is_action_just_pressed("ui_accept") and not GameManager.is_dialogue_active:
 		if GameManager.dia == 1:
-			if not Global.dia1_hablar_mikhail:
-				DialogueManager.show_dialogue_balloon(MIKHAIL_DIA_1_1, "start")
-			elif Global.dia1_hablar_mikhail:
-				DialogueManager.show_dialogue_balloon(MIKHAIL_DIA_1_2, "start")
+			if not Global.dia1_hablar_yerik:
+				DialogueManager.show_dialogue_balloon(YERIK_DIA_1_1, "start")
+			elif Global.dia1_hablar_yerik:
+				DialogueManager.show_dialogue_balloon(YERIK_DIA_1_2, "start")
 		elif GameManager.dia == 2:
-			if not Global.dia2_hablar_mikhail:
-				DialogueManager.show_dialogue_balloon(MIKHAIL_DIA_2_1, "start")
-			elif Global.dia2_hablar_mikhail:
-				DialogueManager.show_dialogue_balloon(MIKHAIL_DIA_2_2, "start")
+			if not Global.dia2_hablar_yerik:
+				DialogueManager.show_dialogue_balloon(YERIK_DIA_2_1, "start")
+			elif Global.dia2_hablar_yerik:
+				DialogueManager.show_dialogue_balloon(YERIK_DIA_2_2, "start")
 		elif GameManager.dia == 3:
-			if not Global.dia3_hablar_mikhail:
-				DialogueManager.show_dialogue_balloon(MIKHAIL_DIA_3_1, "start")
-			elif Global.dia3_hablar_mikhail:
-				DialogueManager.show_dialogue_balloon(MIKHAIL_DIA_3_2, "start")
+			if not Global.dia3_hablar_yerik:
+				DialogueManager.show_dialogue_balloon(YERIK_DIA_3_1, "start")
+			elif Global.dia3_hablar_yerik:
+				DialogueManager.show_dialogue_balloon(YERIK_DIA_3_2, "start")
 		elif GameManager.dia == 4:
-			if not Global.dia4_hablar_mikhail:
-				DialogueManager.show_dialogue_balloon(MIKHAIL_DIA_4_1, "start")
-			elif Global.dia4_hablar_mikhail:
-				DialogueManager.show_dialogue_balloon(MIKHAIL_DIA_4_2, "start")
+			if not Global.dia4_hablar_yerik:
+				DialogueManager.show_dialogue_balloon(YERIK_DIA_4_1, "start")
+			elif Global.dia4_hablar_yerik:
+				DialogueManager.show_dialogue_balloon(YERIK_DIA_4_2, "start")
 		elif GameManager.dia == 5:
-			if not Global.dia5_hablar_mikhail:
-				DialogueManager.show_dialogue_balloon(MIKHAIL_DIA_5_1, "start")
-			elif Global.dia5_hablar_mikhail:
-				DialogueManager.show_dialogue_balloon(MIKHAIL_DIA_5_2, "start")
+			if not Global.dia5_hablar_yerik:
+				DialogueManager.show_dialogue_balloon(YERIK_DIA_5_1, "start")
+			elif Global.dia5_hablar_yerik:
+				DialogueManager.show_dialogue_balloon(YERIK_DIA_5_2, "start")
 		elif GameManager.dia == 6:
-			if not Global.dia6_hablar_mikhail:
-				DialogueManager.show_dialogue_balloon(MIKHAIL_DIA_6_1, "start")
-			elif Global.dia6_hablar_mikhail:
-				DialogueManager.show_dialogue_balloon(MIKHAIL_DIA_6_2, "start")
+			if not Global.dia6_hablar_yerik:
+				DialogueManager.show_dialogue_balloon(YERIK_DIA_6_1, "start")
+			elif Global.dia6_hablar_yerik:
+				DialogueManager.show_dialogue_balloon(YERIK_DIA_6_2, "start")
 		elif GameManager.dia == 7:
-			if not Global.dia7_hablar_mikhail:
-				DialogueManager.show_dialogue_balloon(MIKHAIL_DIA_7_1, "start")
-			elif Global.dia7_hablar_mikhail:
-				DialogueManager.show_dialogue_balloon(MIKHAIL_DIA_7_2, "start")
+			if not Global.dia7_hablar_yerik:
+				DialogueManager.show_dialogue_balloon(YERIK_DIA_7_1, "start")
+			elif Global.dia7_hablar_yerik:
+				DialogueManager.show_dialogue_balloon(YERIK_DIA_7_2, "start")
 
 
 func _on_area_entered(area: Area2D) -> void:
