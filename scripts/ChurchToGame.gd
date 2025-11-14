8d9f1de7becbe265a86d6bcb1b34896db3e248d1
victormillanpin <victor.millan.pin@estudiantat.upc.edu>
2025-11-14 13:47:56 +0100

    puto miquel

--- scripts/ChurchToGame.gd
+++ scripts/ChurchToGame.gd
@@ -5,6 +5,7 @@ extends Area2D
 @export var player_target_position: Vector2
 
 
+
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		SceneManager.change_scene_with_player_position(next_scene_path, player_target_position)
