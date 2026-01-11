46abaff30018ff4ad0a34271656d50084af6303d
holatrolaalola <hugo.bosque@estudiantat.upc.edu>
2026-01-11 11:04:29 +0100

    actu

--- scripts/dia_5_cosa_victor.gd
+++ scripts/dia_5_cosa_victor.gd
@@ -1,22 +1,25 @@
 extends Area2D
 
 
+@onready var sprite_2d: Sprite2D = $Sprite2D
 
 var player_in_area = false
 
 func _ready():
-	
+	sprite_2d.visible = false
 	connect("body_entered", Callable(self, "_on_body_entered"))
 	connect("body_exited", Callable(self, "_on_body_exited"))
 
 func _on_body_entered(area) -> void:
 	if area.name == "Player":
 		player_in_area = true
+		sprite_2d.visible = true
 
 
 func _on_body_exited(area) -> void:
 	if area.name == "Player":
 		player_in_area = false
+		sprite_2d.visible = false
 
 
 func _process(_delta):
