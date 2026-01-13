968262fc6cfe76b3d9cd930f6f41ae2fcbf8be69
holatrolaalola <hugo.bosque@estudiantat.upc.edu>
2026-01-13 11:22:34 +0100

    puto victor no hace, una cosa quetenia quehacer y la he hecho yo porque no sabe preguntar bien al chatgpt

--- scripts/dia_5_cosa_victor.gd
+++ scripts/dia_5_cosa_victor.gd
@@ -1,41 +1,65 @@
-extends Node2D
+extends Area2D
 
-@onready var collision_shape: CollisionShape2D = $CollisionShape2D2
-@onready var sprite_exclamacion: Sprite2D = $Sprite2D
-@onready var papel_animated: AnimatedSprite2D = $AnimatedSprite2D
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var character_body_2d: CharacterBody2D = $CharacterBody2D
 
 const SoldadoMuerto = preload("uid://cnfewcryynldq")
-
-var player_in_area := false
-var dialogo_lanzado := false
-
+var player_in_area = false
+var soldado_muerto_hablado = false
 func _ready():
-	sprite_exclamacion.visible = false
+	sprite_2d.visible = false
 
-func _process(_delta):
-	if GameManager.dia != 5:
-		return
+	# Solo activo el personaje el día 5
+	var activo := GameManager.dia == 5
 
-	# Detectar si el jugador está “cerca” del nodo (simulando área)
-	var players = get_tree().get_nodes_in_group("Player")
-	if players.size() > 0:
-		var player_body = players[0]
-		if player_body.global_position.distance_to(global_position) < 50:  # ajusta radio
-			if not player_in_area:
-				player_in_area = true
-				sprite_exclamacion.visible = true
-		else:
-			if player_in_area:
-				player_in_area = false
-				sprite_exclamacion.visible = false
+	animated_sprite_2d.visible = activo
+	character_body_2d.visible = activo
+	character_body_2d.set_physics_process(activo)
+	character_body_2d.set_process(activo)
 
-	# Interacción
-	if player_in_area and not dialogo_lanzado and not GameManager.is_dialogue_active:
-		if Input.is_action_just_pressed("interact"):
-			_iniciar_dialogo()
+	# Desactivar colisiones si no es día 5
+	for child in character_body_2d.get_children():
+		if child is CollisionShape2D:
+			child.disabled = not activo
 
-func _iniciar_dialogo():
-	dialogo_lanzado = true
-	sprite_exclamacion.visible = false
+	connect("body_entered", Callable(self, "_on_body_entered"))
+	connect("body_exited", Callable(self, "_on_body_exited"))
+
+func _on_body_entered(area) -> void:
+	if GameManager.dia == 5:
+		if area.name == "Player":
+			player_in_area = true
+			sprite_2d.visible = true
+
+func _on_body_exited(area) -> void:
+	if GameManager.dia == 5:
+		if area.name == "Player":
+			player_in_area = false
+			sprite_2d.visible = false
+	
+func _process(delta):
+	if player_in_area and Input.is_action_just_pressed("accept") and not GameManager.is_dialogue_active and not soldado_muerto_hablado:
+		DialogueManager.show_dialogue_balloon(SoldadoMuerto, "start")
+		soldado_muerto_hablado = true
+		desactivar_soldado()
+		
+func _on_dialogue_started(dialogue):
 	GameManager.is_dialogue_active = true
-	DialogueManager.show_dialogue_balloon(SoldadoMuerto, "start")
+
+func _on_dialogue_ended(dialogue):
+	await get_tree().create_timer(0.2).timeout
+	GameManager.is_dialogue_active = false
+
+	
+func desactivar_soldado():
+	sprite_2d.visible = false
+	animated_sprite_2d.visible = false
+	player_in_area = false
+
+	for child in character_body_2d.get_children():
+		if child is CollisionShape2D:
+			child.disabled = true
+
+	character_body_2d.set_physics_process(false)
+	character_body_2d.set_process(false)
