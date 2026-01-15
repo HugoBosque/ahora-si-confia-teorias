8673d3820177f89311f537ed900780c46a17ec39
holatrolaalola <hugo.bosque@estudiantat.upc.edu>
2026-01-15 21:06:57 +0100

    botones funcionan

---
+++ scripts/teorias.gd
@@ -0,0 +1,20 @@
+extends Control
+
+@onready var house_scene = preload("res://scenes/House.tscn")
+@onready var town_scene = preload("res://scenes/game.tscn")
+
+@onready var button: Button = $Button
+
+
+
+func _ready() -> void:
+
+	button.pressed.connect(_on_button_pressed)
+
+
+
+
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/dias/Agradecimientos.tscn")
